37f9405eb5ba72110aa0b46a1f8515cb0acf80e1
Clayton Hughes <clayton@vthera.com>
2022-12-03 23:28:43 -0800

    Implement finding top 3 elves
    I did this in a ridiculous way, as the comments indicate. But it helped me learn
    something about assignment/movement, borrowing, and if statements in general.
    
    So I'm glad I learned something along the way.

--- src/bin/puzzle_01.rs
+++ src/bin/puzzle_01.rs
@@ -39,14 +39,53 @@ fn largest_elf_calories(lines: &Vec<String>) -> (usize, u32) {
     }
 
     let (largest, amount) = elves.iter().enumerate() // gives a tuple (index: usize, value: T)
-        .max_by(|(_, a), (_, b)| a.cmp(b)) // method is different than examples on SO. Closure takes two (usize, T) and returns a std::cmp::Ordering (not a bool)
+//         .max_by(|(_, a), (_, b)| a.cmp(b)) // method is different than examples on SO. Closure takes two (usize, T) and returns a std::cmp::Ordering (not a bool)
+        .max_by_key(|&(_, value)| value)    // oh this is almost certainly what I saw on SO and didn't notice the `_key` in the method name.
         .unwrap();
 
     (largest + 1, *amount)  // 1-indexed. I don't know why the dereference here is needed =(
 }
 
 fn largest_3_elf_calories(lines: &Vec<String>) -> ([usize; 3], u32) {
-    panic!("implement me");
+    let mut elves = Vec::new();
+    let mut calories: u32 = 0;
+    for line in lines {
+        match &line[..] {
+            "" => {
+                elves.push(calories);
+                calories = 0;
+            }
+            _ => {
+                let parsed = match line.parse::<u32>() {
+                    Err(why) => panic!("Couldn't parse int from {}: {}", line, why),
+                    Ok(number) => number,
+                };
+                calories += parsed;
+            }
+        }
+    }
+
+    // I know this is ridiculous, but I don't know the language and it solves the problem
+    let mut largest = Elf::init();
+    let mut largest_2 = Elf::init();
+    let mut largest_3 = Elf::init();
+
+    for (elf, &calories) in elves.iter().enumerate() { // ... I don't know why the (&) borrow(?) is needed here.
+        if largest.calories < calories {    // oh no! my c-style if parens
+            largest_3 = largest_2;
+            largest_2 = largest;
+            largest = Elf::new(elf, calories);
+        } else if largest_2.calories < calories {
+            largest_3 = largest_2;
+            largest_2 = Elf::new(elf, calories);
+        } else if largest_3.calories < calories {
+            largest_3 = Elf::new(elf, calories);
+        }
+    }
+    // ... I'm a doofus: I could've just sorted it.
+
+    ([largest.elf + 1, largest_2.elf + 1, largest_3.elf + 1], // 1-indexed.
+    largest.calories + largest_2.calories + largest_3.calories)
 }
 
 fn main() {
